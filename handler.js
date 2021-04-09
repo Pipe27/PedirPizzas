7a4c10ef0b1f26949cbb4d8296f9a162739c2b9f
Felipe <felipea.97@hotmail.com>
2021-04-09 14:59:49 -0500

    first commit

---
+++ handler.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
+const orderedPizzasMetadataManager = require('./orderedPizzasMetadataManager');
+
+var sqs = new AWS.SQS({ region: process.env.REGION });
+const QUEUE_URL = process.env.PENDING_ORDER_QUEUE;
+
+module.exports.hacerPedido = (event, context, callback) => {
+    const body = JSON.parse(event.body);
+    const orderedPizza = {
+        orderId: uuidv4(),
+        name: body.name,
+        address: body.address,
+        pizzas: body.pizzas,
+        date: Date.now()
+    };
+    const params = {
+        MessageBody: JSON.stringify(orderedPizza),
+        QueueUrl: QUEUE_URL
+    };
+
+    sqs.sendMessage(params, function(err, data) {
+        if (err) {
+            sendResponse(500, err, callback);
+        } else {
+            const message = {
+                order: orderedPizza,
+                messageId: `El mensaje tiene el id: ${data.MessageId}`
+            };
+            sendResponse(200, message, callback);
+        }
+    });
+};
+
+module.exports.prepararPedido = (event, context, callback) => {
+    console.log('Se está preparando el pedido');
+    const orderedPizza = JSON.parse(event.Records[0].body);
+    orderedPizzasMetadataManager.saveCompletedOrder(orderedPizza).then(data => {
+        callback();
+    }).catch(err => {
+        callback(err);
+    });
+    callback();
+};
+
+module.exports.enviarPedido = (event, context, callback) => {
+    console.log('Se va a enviar el pedido');
+    const record = event.Records[0];
+    if (record.eventName == 'INSERT') {
+        console.log('deliverOrder');
+        const orderId = record.dynamodb.Keys.orderId.S;
+        orderedPizzasMetadataManager.deliverOrder(orderId).then(data => {
+            console.log(data);
+            callback();
+        }).catch(err => {
+            callback(err);
+        });
+    } else {
+        console.log('No es un pedido nuevo');
+        callback();
+    }
+};
+
+module.exports.estadoPedido = (event, context, callback) => {
+    console.log('Consultando pedido');
+    const orderId = event.pathParameters && event.pathParameters.orderId;
+    if (orderId !== null) {
+        orderedPizzasMetadataManager.getOrder(orderId).then(order => {
+            sendResponse(200, `La orden: ${orderId} se encuentra en estado: ${order.delivery_status}`, callback);
+        }).catch(error => {
+            sendResponse(500, 'Hubo un error al procesar el pedido', callback);
+        });
+    } else {
+        sendResponse(400, 'No se envió un orderId para consultar', callback);
+    }
+};
+
+function sendResponse(statusCode, message, callback) {
+    const response = {
+        statusCode: statusCode,
+        body: JSON.stringify(message)
+    };
+    callback(null, response);
+}
---
+++ orderedPizzasMetadataManager.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+module.exports.saveCompletedOrder = orderedPizzas => {
+    orderedPizzas.delivery_status = 'READY FOR DELIVERY';
+    const params = {
+        TableName: process.env.ORDERED_PIZZAS_TABLE,
+        Item: orderedPizzas
+    };
+    console.log('Se almacenó la orden en base de datos');
+    return dynamo.put(params).promise();
+};
+
+module.exports.deliverOrder = orderId => {
+    console.log('Enviar una orden fue llamado');
+    const params = {
+        TableName: process.env.ORDERED_PIZZAS_TABLE,
+        Key: {
+            orderId
+        },
+        ConditionExpression: 'attribute_exists(orderId)',
+        UpdateExpression: 'set delivery_status = :newState',
+        ExpressionAttributeValues: {
+            ':newState': 'DELIVERED'
+        },
+        ReturnValues: 'ALL_NEW'
+    };
+
+    return dynamo.update(params).promise().then(response => {
+        console.log('Orden entregada');
+        return response.Attributes;
+    });
+};
+
+module.exports.getOrder = orderId => {
+    console.log('Obtener una orden fue llamado');
+
+    const params = {
+        TableName: process.env.ORDERED_PIZZAS_TABLE,
+        Key: {
+            orderId
+        }
+    };
+
+    return dynamo.get(params).promise().then(item => {
+        return item.Item;
+    });
+};
